b99f1cf3c20499895ab3037eddb415319769a743
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-08-18 20:46:43 +0600

    added two number sume problem

--- variable.js
+++ variable.js
@@ -1,5 +1,11 @@
-name = "Shariful";
-age = 22;
+// name = "Shariful";
+// age = 22;
 
-console.log("My name is " + name + " and I am " + age + " years old.");
+// console.log("My name is " + name + " and I am " + age + " years old.");
 
+
+a = 20;
+b = 10;
+let sum = a + b ;
+
+console.log(sum);
