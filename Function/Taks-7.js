c2869376dd29c15cb2403169c44db3b3845e39e5
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-08-24 20:52:23 +0600

    added taks 7

---
+++ Function/Taks-7.js
@@ -0,0 +1,19 @@
+
+
+// Problem 7 — Calculate Electricity Bill ⚡
+
+
+function calculateBill(units, pricePerUnit){
+ 
+    const totalBill = units * pricePerUnit ;
+
+
+    return totalBill;
+
+}
+
+
+const bill = calculateBill(150, 8);
+
+console.log(bill);
+
