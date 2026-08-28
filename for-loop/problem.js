dcf084d910cfc90db744d7e00099a695c53b6b59
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-08-28 20:46:57 +0600

    solved a odd number problem

--- for-loop/problem.js
+++ for-loop/problem.js
@@ -9,10 +9,23 @@
 
 // problem 2
 
-for (let i = 1; i <= 20; i++ ){
+// for (let i = 1; i <= 20; i++ ){
 
-   if (i % 2 === 0){
-    console.log(i);
-   }
+//    if (i % 2 === 0){
+//     console.log(i);
+//    }
 
-}
+// }
+
+
+// problem 3
+
+
+// for ( let i = 1; i <=20; i++ ){
+
+//     if (i % 2 === 1 ){
+//         console.log(i);
+//     }
+// }
+
+
