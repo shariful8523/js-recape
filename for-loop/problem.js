4e8289f37a68b72c7d0992527edd68c03141722f
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-08-28 20:58:55 +0600

    added new problem

--- for-loop/problem.js
+++ for-loop/problem.js
@@ -29,3 +29,37 @@
 // }
 
 
+// problem 4
+
+// let sum = 0;
+
+
+// for ( let i = 1; i <= 10; i++) {
+
+
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+
+// const number = 5;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(number + " x " + i + " = " + (number * i));
+// }
+
+// let count = 0;
+
+// for ( let i = 1; i <= 20; i++){
+
+//     if ( i % 2 === 0){
+       
+//         count++;
+//     }
+
+// }
+
+// console.log(count);
+
+
