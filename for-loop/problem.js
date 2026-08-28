2847cc7a427c928e6e60584d8c9853cd218ad60f
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-08-28 20:42:11 +0600

    added for loop problem

---
+++ for-loop/problem.js
@@ -0,0 +1,18 @@
+
+
+// problem 1
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// problem 2
+
+for (let i = 1; i <= 20; i++ ){
+
+   if (i % 2 === 0){
+    console.log(i);
+   }
+
+}
