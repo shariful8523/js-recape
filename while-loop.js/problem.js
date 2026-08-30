ff119a983224a28abbab43979fa56329b5f4b958
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-08-30 12:28:14 +0600

    added the more solved problem topic is while loop

--- while-loop.js/problem.js
+++ while-loop.js/problem.js
@@ -1,9 +1,49 @@
 
 
 
-let i = 1; 
+// // // let i = 1; 
 
-while ( i <= 5 ){
-    console.log(i);
+// // // while ( i <= 5 ){
+// // //     console.log(i);
+// // //     i++;
+// // // }
+
+
+// // let i = 10 ;
+
+// // while ( i >= 1 ){
+
+// //     console.log(i);
+
+// //     i--;
+// // };
+
+
+// let i = 1;
+
+// while ( i <= 20){
+
+//     if ( i % 2 === 0){
+   
+//      console.log(i);
+
+//     }
+
+//     i++;
+// }
+
+
+
+let i = 1;
+let sum = 0;
+
+while (i <= 20) {
+
+    if (i % 2 === 1) {
+        sum = sum + i;
+    }
+
     i++;
 }
+
+console.log(sum);
