3435dba6446a18116fe4bcde5b3e0c18445d0470
Md. Shariful Islam <mdsharifulislam8523@gmail.com>
2026-09-01 21:46:31 +0600

    added a solved problem with quesions

--- First-class-function.js/practice.js
+++ First-class-function.js/practice.js
@@ -1,16 +1,32 @@
 
 
-function great() {
-    console.log("Hello shariful");
-}
+// function great() {
+//     console.log("Hello shariful");
+// }
 
-function executeFunction(fn){
+// function executeFunction(fn){
    
-   fn();
+//    fn();
 
 
+// }
+
+// let result = executeFunction(great);
+
+// console.log(result);
+
+
+// add function-কে calculate()-এর argument হিসেবে পাঠাবে।
+// calculate() এর ভিতরে operation ব্যবহার করে দুইটি number-এর যোগফল বের করবে।
+
+function add(a,b){
+    return a + b;
 }
 
-let result = executeFunction(great);
+function calculate(a,b, operation){
+    return operation(a,b)
+}
+
+const result = calculate(10,20,add)
 
 console.log(result);
